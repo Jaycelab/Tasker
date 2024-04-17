2cf919e74ab42c4dd69c96f9fa71fa75d7044ec3
jaycelab <jayxalvarado@gmail.com>
2024-04-17 06:03:45 -0700

    finished add tasks, enter key and scantask list methods

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 //overall task object
 const tasker = {
-    //construct
-    construct: function() {
+  //construct
+  construct: function () {
     this.selectElements();
     this.bindEvents();
     this.scanTaskList();
   },
   //task properties
   selectElements: function () {
-    this.taskInput = document.getElementById("input-task");
-    this.taskList = document.getElementById("tasks");
+    this.taskInput = document.querySelector("#input-task");
+    this.taskList = document.querySelector("#tasks");
 
     //selects all list items within parent UL
     this.taskListChildren = this.taskList.children;
-    this.addButton = document.getElementById("add-task-btn");
-    this.errorMessage = document.getElementById("error")
+    //add tasks button
+    this.addButton = document.querySelector("#add-task-btn");
+    //error message
+    this.errorMessage = document.querySelector("#error");
   },
 
   //buildtask
@@ -35,10 +37,10 @@ const tasker = {
 
     //trash icon utilizing font awesome
     taskTrash = document.createElement("i");
-    taskTrash.setAttribute("class","fa fa-trash");
+    taskTrash.setAttribute("class", "fa fa-trash");
 
     //insert trash icon inside button using append child
-   taskButton.appendChild(taskTrash);
+    taskButton.appendChild(taskTrash);
 
     //append elements to task list
     taskListItem.appendChild(taskCheckbox);
@@ -49,25 +51,50 @@ const tasker = {
     this.taskList.appendChild(taskListItem);
   },
 
-    //error check 
-    error: function() {
+  //error check. Default style is set to none, switching to block will display error msg
+  error: function () {
     this.errorMessage.style.display = "block";
   },
 
-
-    //add task
-    addTask: function() {
+  //add task method. Adds new tasks to list as opposed to build tasks (builds the HTML)
+  addTask: function () {
     let taskValue = this.taskInput.value;
     this.errorMessage.style.display = "none";
-    
-    if(taskValue === ""){
+
+    //test if task value empty string , run error func
+    if(taskValue === "") {
       this.error();
-    }
-    else {
-      this.buildTask();
+    } else {
+      //else run buildTask & set task input to empty string  
+      this.buildtask();
       this.taskInput.value = "";
-      this.scanTaskList();
+      //this.scanTaskList();
     }
+  },
+
+  //enterKey method
+    enterKey: function(event){
+    if (event.keyCode === 13 || event.which === 13 ){
+      this.addTask();
+    }
+  },
+
+  //scanTaskList method
+  scanTaskList: function() {
+    let taskListItem, checkBox, deleteButton;
+
+    //loop through all list elements
+    for (i = 0; i < this.taskListChildren.length; i++){
+      taskListItem = this.taskListChildren[i];
+      //select checkbox and delete button
+      checkBox = taskListItem.getElementsByTagName("input")[0];
+      deleteButton = taskListItem.getElementsByTagName("button")[0];
+
+      //bind onclick event to the checkbox
+      checkBox.onclick = this.completeTask.bind(this, taskListItem, checkBox);
+
+      //add click event to the delete button
+      deleteButton.onclick = this.deleteTask.bind(this, i);
+    }
+  },
 };
-
-
